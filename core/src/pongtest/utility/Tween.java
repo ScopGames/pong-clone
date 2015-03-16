8c6ff6e130995e44d853ffef13f4b36daad9461e
Davide Cristini <davcri91@gmail.com>
2015-03-16 21:19:07 +0100

    Improved Tween utility class

--- core/src/pongtest/utility/Tween.java
+++ core/src/pongtest/utility/Tween.java
@@ -2,6 +2,7 @@ package pongtest.utility;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.MathUtils;
 
 /** Interpolates values using differents easing equations.
  * 
@@ -14,7 +15,12 @@ public class Tween {
 	float value = 0;
 	private boolean finished = false;
 	Interpolation interpolation;
+	RepeatMode repeatMode = RepeatMode.NORMAL;
 
+	public enum RepeatMode {
+		NORMAL, LOOP, LOOP_PINGPONG, REPEAT, REPEAT_PINGPONG
+	};
+	
 	/** Creates a new tween.
 	 * 
 	 * @param interpolation Equation used for the interpolation.
@@ -32,19 +38,64 @@ public class Tween {
 	 * 
 	 * @param delta Frame time. */
 	public void update (float delta) {
-		elapsed += delta;
-		float percentage = elapsed / duration;
-		
-		if (percentage > 1)
+		if (!finished)
 		{
-			finished = true;
-			percentage = 1;
-		}
+			elapsed += delta;
+			float percentage = elapsed / duration;
+			percentage = MathUtils.clamp(percentage, 0, 1);
+
+			//
+			finished = updateMode(percentage);
+		}				
+	}
+	
+	/**
+	 * Updates the Tween according to the specified repeatMode.
+	 * 
+	 * @param percentage value between 0 and 1. 
+	 * @return true if the tween is finished, false otherwise.
+	 */
+	private boolean updateMode(float percentage)
+	{
+		boolean modeFinished = false;
+		
+		switch (repeatMode) {
+			case NORMAL:
+				if (percentage  >= 1)
+					modeFinished = true;
+				break;
 			
+			case LOOP: // infinite loop
+				if (percentage >= 1) {	
+					reset();
+				}
+				break;
+			
+			case LOOP_PINGPONG: // infinite pingpong loop
+				if (percentage >= 1) {			
+					
+					Gdx.app.log("stat", "initial: " + initialValue + "\nfinalvalue: " 
+							+ finalValue + "actual:" + value);
+					// swapping initial and final values
+					float tmp = initialValue;
+					initialValue = finalValue;
+					finalValue = tmp;
+					
+					reset();
+					percentage = 0;
+					Gdx.app.log("stat", "initial: " + initialValue + "\nfinalvalue: " 
+					+ finalValue + "actual:" + value);
+				}				
+				break;
+			
+			default:
+				break;
+		}
 		
 		value = interpolation.apply(initialValue , finalValue, percentage);
+		return modeFinished;
 	}
-
+	
 	/** Gets the current tween value.
 	 * 
 	 * @return Value of the tween. */
@@ -61,5 +112,18 @@ public class Tween {
 
 	public boolean finished() {
 		return finished;
+	}
+	
+	/**
+	 * Gets the percentage of completion.
+	 * 
+	 * @return value between 0 and 1.
+	 */
+	public float getPercentage() {
+		return elapsed/duration;
+	}
+	
+	public void setRepeatMode(RepeatMode repeatMode) {
+		this.repeatMode = repeatMode;
 	}
 }
