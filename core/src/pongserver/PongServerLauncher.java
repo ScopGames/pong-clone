369c5ba5c3472ca229f2b5e5749860ce1bb2d39a
Davide Cristini <davcri91@gmail.com>
2015-03-31 12:56:51 +0200

    Little changes.

--- core/src/pongserver/PongServerLauncher.java
+++ core/src/pongserver/PongServerLauncher.java
@@ -6,17 +6,21 @@ import java.net.SocketException;
 import pongserver.network.PongServer;
 
 
-public class Main {
+public class PongServerLauncher {
 
 	public static void main(String[] args) {
 		try 
 		{
 			PongServer server = new PongServer();	
 			
-			while(true)
+			server.start();
+			
+			while(true) // change this
 			{
 				server.listen();
 			}
+			
+			//server.stop();
 		} 
 		catch (SocketException e) 
 		{
