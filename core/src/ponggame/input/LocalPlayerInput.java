26a002fafaedfa5c9ca79775c6a6baec801690d8
Davide Cristini <davcri91@gmail.com>
2015-04-22 12:40:50 +0200

    Added LocalPlayerInput

---
+++ core/src/ponggame/input/LocalPlayerInput.java
@@ -0,0 +1,24 @@
+package ponggame.input;
+
+import ponggame.entity.Paddle;
+
+public class LocalPlayerInput extends PlayerInput {
+
+	public LocalPlayerInput(Paddle paddle, layoutInput layout) 
+	{
+		super(paddle, layout);
+	}
+
+	@Override
+	void onDownKeyPressed(float delta) 
+	{
+		paddle.moveDown(delta);
+	}
+
+	@Override
+	void onUpKeyPressed(float delta) 
+	{
+		paddle.moveDown(delta);
+	}
+
+}
