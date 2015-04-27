9e25ff515c74478962dba1f65143375af500e090
Pasquale <pasquale.salvati91@gmail.com>
2015-04-27 15:56:31 +0200

    create Data Class (maybe needs a refactor too generic)

---
+++ core/src/pongserver/utility/Data.java
@@ -0,0 +1,55 @@
+package pongserver.utility;
+
+import java.io.Serializable;
+
+import ponggame.entity.GameEntity;
+import pongserver.utility.NetworkHelper.Task;
+
+public class Data implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Task mTask;
+	//private GameEntity mGameEntity;
+	
+	
+	public Data(Task task){
+		
+		this.mTask = task;
+	}
+	
+	/*public Data(Task task, GameEntity game){
+
+		this(task);
+		this.mGameEntity = game;
+	}
+	
+	/public void updateGame(GameEntity g )
+	{
+		this.mGameEntity = g;
+	}*/
+	
+	public void setTask(Task t){
+		
+		this.mTask = t;
+	}	
+	
+	public Task getTask(){
+		return mTask;
+	}
+
+	/*public GameEntity getGameEntity(){
+		return mGameEntity;
+	}
+
+	public void setGameEntity(GameEntity mGameEntity) {
+		this.mGameEntity = mGameEntity;
+	}*/
+
+	
+
+
+}
+
