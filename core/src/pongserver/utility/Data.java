a1563b7f4e0eb2762fa31abea290f0372f30f803
Pasquale <pasquale.salvati91@gmail.com>
2015-05-04 16:12:02 +0200

    added time in class data

--- core/src/pongserver/utility/Data.java
+++ core/src/pongserver/utility/Data.java
@@ -1,6 +1,8 @@
 package pongserver.utility;
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
 
 import ponggame.entity.GameEntity;
 import pongserver.utility.NetworkHelper.Task;
@@ -13,11 +15,14 @@ public class Data implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Task mTask;
 	private GameEntity mGameEntity;
+	private Date time;
 	
 	
 	public Data(Task task)
 	{	
+		Calendar cal = Calendar.getInstance();
 		this.mTask = task;
+		this.setTime(cal.getTime());
 	}
 	
 	public Data(Task task, GameEntity game)
@@ -41,6 +46,14 @@ public class Data implements Serializable {
 
 	public void setGameEntity(GameEntity mGameEntity) {
 		this.mGameEntity = mGameEntity;
+	}
+
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
 	}
 
 	
