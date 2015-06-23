6c5a93caedb1b36c94c1bb49285d7c4ceb666570
Davide Cristini <davcri91@gmail.com>
2015-06-23 10:41:11 +0200

    Bug fix

--- core/src/pongserver/network/PongServer.java
+++ core/src/pongserver/network/PongServer.java
@@ -182,7 +182,7 @@ public class PongServer
 							}
 							
 							// if both players are connected and playing
-							if(playersAck.get(0) && playersAck.get(0))
+							if(playersAck.get(0) && playersAck.get(1))
 								this.cancel(); // cancel this task
 						}
 					}, 0, 250);
