8e3704fac2de91119e79f46e9c93bc146d38f670
Pasquale <pasquale.salvati91@gmail.com>
2015-06-24 14:50:12 +0200

    added some javadoc

--- core/src/ponggame/Main.java
+++ core/src/ponggame/Main.java
@@ -1,3 +1,9 @@
+/**
+ * Main.java our main class 
+ * 
+ * It manages the Screens in our application
+ * 
+ */
 package ponggame;
 
 import java.net.DatagramSocket;
@@ -12,14 +18,25 @@ import com.badlogic.gdx.Gdx;
 
 public class Main extends Game
 {
+	/**
+	 * public enum Screens contains the several screen we can have in the application
+	 */
 	public enum Screens {PONGGAME, MAINSCREEN, MULTIPLAYER_PONG_GAME};
 	
+	/**
+	 * @see changeScreen
+	 */
 	@Override
 	public void create() 
 	{	
 		changeScreen(Screens.MAINSCREEN);
 	}
 	
+	/**
+	 * Set the screen which is requested 
+	 * 
+	 * @param newScreen
+	 */
 	public static void changeScreen(Screens newScreen)
 	{
 		switch(newScreen)
@@ -36,6 +53,13 @@ public class Main extends Game
 		}		
 	}
 	
+	/**
+	 * Starts the game in multiplayer mode 
+	 * 
+	 * @param socket server's socket
+	 * @param server server's ip
+	 * @param isPaddleLeft a boolean indicating the side of player's paddle
+	 */
 	public static void startMultiplayerPong(DatagramSocket socket, 
 			NetworkNode server, 
 			boolean isPaddleLeft)
